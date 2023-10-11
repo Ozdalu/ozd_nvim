45ab15c2e243490bbf5a89d15a87868a8b9e0e3a
Ozdalu <ozdalu.git.retold740@passmail.net>
2023-10-11 10:19:25 +0200

    first commit

---
+++ lua/ormaazd/colors.lua
@@ -0,0 +1,27 @@
+OrmaazdColors = {
+	green		= '#27e73a',
+	black		= '#262626',
+	darkgrey	= '#4b4b4b',
+	grey		= '#7a7a7a',
+	white		= '#f8f8f2',
+	reds = {
+		light	= '#ee0d0d',
+		neutral	= '#550818',
+		dark	= '#2b0409',
+	},
+	blues = {
+		light	= '#7bf6d5',
+		neutral = '#3342f4',
+		dark	= '#072384',
+	},
+	oranges = {
+		light	= '#efff3e',
+		neutral	= '#ffb80a',
+		dark	= '#661608',
+	},
+	purples = {
+		light	= '#b47eff',
+		neutral	= '#4f1d81',
+		dark	= '#290040',
+	},
+}
---
+++ lua/ormaazd/highlight.lua
@@ -0,0 +1,17 @@
+vim.api.nvim_set_hl(0, "Pmenu", { bg = OrmaazdColors.purples.neutral })
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+vim.api.nvim_set_hl(0, "CursorLine", { bg = OrmaazdColors.purples.dark })
+
+
+function LualineHighlights()
+	vim.api.nvim_set_hl(0, "lualine_b_diff_modified_insert", { fg = OrmaazdColors.blues.dark, bg = OrmaazdColors.oranges.neutral})
+	vim.api.nvim_set_hl(0, "lualine_b_diff_added_insert", { fg = '#007821', bg = OrmaazdColors.oranges.neutral })
+
+	vim.api.nvim_set_hl(0, "lualine_b_diff_removed_normal", { fg = OrmaazdColors.blues.light, bg = OrmaazdColors.purples.neutral })
+
+	vim.api.nvim_set_hl(0, "lualine_b_diagnostic_warn_insert", { fg = OrmaazdColors.reds.dark, bg = OrmaazdColors.oranges.neutral })
+	vim.api.nvim_set_hl(0, "lualine_b_diagnostic_info_insert", { fg = OrmaazdColors.blues.dark, bg = OrmaazdColors.oranges.neutral })
+	vim.api.nvim_set_hl(0, "lualine_b_diagnostic_hint_insert", { fg = OrmaazdColors.purples.dark, bg = OrmaazdColors.oranges.neutral })
+end
