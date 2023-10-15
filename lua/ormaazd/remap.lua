227454a365c339015fd3c774750b6d902220b2c5
Ozdalu <ozdalu.git.retold740@passmail.net>
2023-10-16 01:13:27 +0200

    feat(remap): add C-k & C-j for pageup & pagedown

--- lua/ormaazd/remap.lua
+++ lua/ormaazd/remap.lua
@@ -1,3 +1,5 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Explore)
---vim.keymap.set("n", "<C-B>", vim.cmd.NvimTreeToggle)
+vim.keymap.set("n", "<C-b>", vim.cmd.NvimTreeToggle)
+vim.keymap.set("n", "<C-j>", "<PageDown>")
+vim.keymap.set("n", "<C-k>", "<PageUp>")
