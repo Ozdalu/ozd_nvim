cbc44b425d716c79e81ebbdb9c53067080e98784
paul.bisset <paul.bisset@juridifeel.com>
2023-10-17 16:27:00 +0200

    feat(highlight): general highlight because of TS

--- lua/ormaazd/remap.lua
+++ lua/ormaazd/remap.lua
@@ -3,3 +3,6 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Explore)
 vim.keymap.set("n", "<C-b>", vim.cmd.NvimTreeToggle)
 vim.keymap.set("n", "<C-j>", "<PageDown>")
 vim.keymap.set("n", "<C-k>", "<PageUp>")
+
+vim.keymap.set("n", "<A-ù>", vim.cmd.FTermOpen)
+vim.keymap.set("t", "<A-ù>", vim.cmd.FTermClose)
