08cd4fe4e006ba1ce99173125bd0423fcdc3c41c
Ormazd <pbisset@protonmail.com>
2023-10-18 23:42:23 +0200

    hotfix(remap): mode for <leader>Y
    Was Visual mode, is now Normal mode

--- lua/ormaazd/remap.lua
+++ lua/ormaazd/remap.lua
@@ -23,4 +23,4 @@ vim.keymap.set("x", "<leader>p", "\"_dP")
 
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
-vim.keymap.set("v", "<leader>Y", "\"+Y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
