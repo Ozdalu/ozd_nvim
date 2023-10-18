eba4789fa294877d668f6f0605f075f518d89a8e
Ozdalu <ozdalu.git.retold740@passmail.net>
2023-10-18 18:56:35 +0200

    fix(fterm): use system shell

--- plugin/fterm.lua
+++ plugin/fterm.lua
@@ -2,6 +2,6 @@ require('FTerm').setup({
 	vim.api.nvim_create_user_command('FTermOpen', require('FTerm').open, { bang = true }),
 	vim.api.nvim_create_user_command('FTermClose', require('FTerm').close, { bang = true }),
 
-	cmd = '/bin/fish',
+	cmd = os.getenv('SHELL'),
     border = 'rounded',
 })
