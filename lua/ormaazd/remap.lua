49f389367d24493365bcde48fe482e5abbd162bb
Ozdalu <ozdalu.git.retold740@passmail.net>
2023-10-19 14:09:49 +0200

    feat(FTerm): better mapping to toggle fterm

--- lua/ormaazd/remap.lua
+++ lua/ormaazd/remap.lua
@@ -6,7 +6,7 @@ vim.keymap.set("n", "<C-b>", vim.cmd.NvimTreeToggle)
 vim.keymap.set("n", "<C-k>", "<PageUp>")
 vim.keymap.set("n", "<C-j>", "<PageDown>")
 
-vim.keymap.set("n", "<A-ù>", vim.cmd.FTermOpen)
+vim.keymap.set("n", "<A-ù>", vim.cmd.FTermToggle)
 vim.keymap.set("t", "<A-ù>", vim.cmd.FTermClose)
 
 vim.keymap.set("n", "<leader>ha", vim.cmd.HarpoonMark)
