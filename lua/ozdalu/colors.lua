6b4dd590f71c7571db593f7f93ecd64482c2a7dd
Ozdalu <ozdalu.git.retold740@passmail.net>
2025-03-24 00:24:50 +0100

    refacto(colors): move string color to OzdaluColors.specials.salmon

--- lua/ozdalu/colors.lua
+++ lua/ozdalu/colors.lua
@@ -3,29 +3,33 @@ OzdaluColors = {
 	black		= '#262626',
 	white		= '#f8f8f2',
   greys = {
-    light		= '#e6e6e6',
+    light		= '#cfcfcf',
     neutral		= '#7a7a7a',
-    dark	= '#3b3b3b',
+    dark	= '#3b3b3b'
   },
 	reds = {
 		light	= '#ee0d0d',
 		neutral	= '#550818',
-		dark	= '#2b0409',
+		dark	= '#2b0409'
 	},
 	blues = {
 		light	= '#40ffff',
 		neutral = '#3342f4',
-		dark	= '#072384',
+		dark	= '#072384'
 	},
 	oranges = {
 		light	= '#efff3e',
 		neutral	= '#ffb80a',
-		dark	= '#661608',
+		dark	= '#661608'
 	},
 	purples = {
 		light	= '#b47eff',
 		neutral	= '#4f1d81',
 		dark	= '#290040',
-		darkest	= '#100119',
-	}
+		darkest	= '#100119'
+	},
+  specials = {
+    blue_grey = '#f0f2f7',
+    salmon = '#ffa0a0'
+  }
 }
