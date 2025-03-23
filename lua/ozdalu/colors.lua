93e023c19dfbcfea6e56eaae6d9628ec265f47a1
Ozdalu <ozdalu.git.retold740@passmail.net>
2025-03-23 21:03:08 +0100

    refacto(colors): move greys to OzdaluColors.greys

--- lua/ozdalu/colors.lua
+++ lua/ozdalu/colors.lua
@@ -1,9 +1,12 @@
 OzdaluColors = {
 	green		= '#27e73a',
 	black		= '#262626',
-	darkgrey	= '#4b4b4b',
-	grey		= '#7a7a7a',
 	white		= '#f8f8f2',
+  greys = {
+    light		= '#e6e6e6',
+    neutral		= '#7a7a7a',
+    dark	= '#3b3b3b',
+  },
 	reds = {
 		light	= '#ee0d0d',
 		neutral	= '#550818',
@@ -24,5 +27,5 @@ OzdaluColors = {
 		neutral	= '#4f1d81',
 		dark	= '#290040',
 		darkest	= '#100119',
-	},
+	}
 }
